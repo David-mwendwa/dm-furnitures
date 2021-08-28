1bfc971b442099f0c82357ca06badc45a8d505c7
David Mwendwa <klatzdavie19@gmail.com>
2021-08-28 10:54:26 +0300

    feat: add about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>Our Story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
+            lacinia orci. Donec laoreet leo ut metus feugiat, eget commodo mi
+            euismod. Integer sit amet pharetra leo. Vestibulum lorem erat,
+            mollis non tellus at, mollis fringilla lorem.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
 }
 
 const Wrapper = styled.section`
